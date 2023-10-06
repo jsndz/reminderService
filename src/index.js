3519fb6f84207db7362e5dfd3286df6a2f8c4721
jsndz <jaisondz9360.com>
2023-10-06 12:00:31 +0530

    Basic setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {PORT} = require('./config/serverconfig')
+
+const setUpServer = () => {
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.listen(PORT,() => {
+        console.log(`Server started at ${PORT}`)
+    })
+}
+
+setUpServer();
