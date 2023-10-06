42b22e9389c53b64cbb2667722e0be9b76567552
jsndz <jaisondz9360.com>
2023-10-06 12:45:16 +0530

    added mailing service

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverconfig')
+const {sendBasicEmail} = require('./services/email-service')
 
 const setUpServer = () => {
     const app = express();
@@ -10,7 +11,14 @@ const setUpServer = () => {
     app.use(bodyParser.urlencoded({extended:true}));
 
     app.listen(PORT,() => {
-        console.log(`Server started at ${PORT}`)
+        console.log(`Server started at ${PORT}`);
+
+        sendBasicEmail(
+            'helloJaison@gmail.com',
+            'jaisondz9360@gmail.com',
+            'testing mail',
+            'hello'
+        );
     })
 }
 
